85cabbc0bd1bf8d4f7e9c2c305eb2d9de4b8e28f
sergipastor <sergipastor@plyzer.com>
2019-10-03 23:49:41 +0200

    Create excceptions.py

---
+++ segurata/enginee/enginee/login/exceptions.py
@@ -0,0 +1,7 @@
+class LoginGeneralError(Exception):
+    'Segurata was able not login.'
+    pass
+
+class NotLoggedInError(LoginGeneralError):
+    'The css selector did not find any matching elements'
+    pass
